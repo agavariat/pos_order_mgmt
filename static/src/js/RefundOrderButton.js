54f2a60c4eacc5825757821118777e7f9e4cdcc5
Juan Arbelaez <desarrollo@intresco.co>
2024-06-12 23:26:18 +0000

    RefundOrderButton.js:
    - Se crea un array vacío para las NCDE asociadas a un DEE.
    
    - Primero se crea la restricción de no poder hacerle una NCDE a una misma NCDE.
    
    - Se hace la validación de que el documento al cual se le va a hacer una NCDE sea un DEE. Dentro de esta validación se hace un llamado rpc a la DB con el dominio related_domain y solicitando los campos related_field.
    
    - Si se encuentra una o varias NCDE relacionada a ese DEE, se pushean al array vacio los valores ($) de las anteriores NCDE
    
    - Se compara el valor pagado de las NCDE anteriores con el valor original de la DEE. Si son iguales, saldrá un aviso que le dira al usuario que la factura (DEE) ya fue devuelta en su totalidad.
    
    - Si el valor pagado de notas anteriores es menor al valor pagado de la factura, saldrá un aviso que le indica al usuario cuanto se ha devuelto de esa factura y cuanto falta por devolver, dejándolo continuar con el proceso.

--- static/src/js/RefundOrderButton.js
+++ static/src/js/RefundOrderButton.js
@@ -21,7 +21,62 @@ odoo.define("pos_order_mgmt.RefundOrderButton", function (require) {
             const refund_order = this.orderManagementContext.selectedOrder;
             if (!refund_order) return;
 
-            var order = this._prepare_order_from_order(refund_order);
+            var refunded_orders = [];
+            var related_domain = [['related_order', '=', refund_order.account_move_name]];
+            var related_field = ['related_order', 'amount_paid']
+
+            // Se asigna la instancia de la clase OrderManagementScreen a una variable que va a contener las ordenes de la base de datos.
+            // const ordenes_database = this.orders
+
+            if(refund_order.document_type == 'ncde'){
+                return this.showPopup('ErrorPopup', {
+                    'title': _t('Nota Crédito'),
+                    'body': _t(`La orden seleccionada es una Nota Crédito, no se puede realizar una devolución.`),
+                    cancel: function(){
+                        this.showScreen('OrderManagementScreen');
+                    },
+                });
+            } 
+            else if (refund_order.document_type === 'dee') {
+                try {
+                    const output = await this.rpc({
+                        model: 'pos.order',
+                        method: 'search_read',
+                        args: [related_domain, related_field]
+                    });           
+                    refunded_orders.push(...output);
+                } catch (error) {
+                    console.error('Error fetching related orders:', error);
+                }
+            }
+            
+            if (refunded_orders.length > 0) {
+
+                let monto_reembolsado = refunded_orders.reduce((total, order) => total + Math.abs(order['amount_paid']), 0);
+                monto_reembolsado = Math.round(monto_reembolsado * 100) / 100;
+                // En monto reembolsado se suman todos los campos amount_paid de las NCDE asociadas al DEE y se redonde a dos decimales.
+                let monto_a_reembolsar = refund_order.paymentlines.models[0].amount;
+                monto_a_reembolsar = Math.round(monto_a_reembolsar * 100) / 100;
+                // En monto a reembolsar se toma el valor del pago total de la orden seleccionada y se redondea a dos decimales.
+
+                if (monto_reembolsado === monto_a_reembolsar) { // Ojo, usar operador de comparacion '===' , no de asignacion.
+                    return this.showPopup('ErrorPopup', {
+                        'title': _t('Nota Crédito'),
+                        'body': _t(`La orden ${refund_order.account_move_name} ya ha sido devuelta en su totalidad.`),
+                        cancel: function(){
+                            this.showScreen('OrderManagementScreen');
+                        },
+                    });
+                }
+                else if (monto_reembolsado < monto_a_reembolsar) {
+                        this.showPopup('ErrorPopup', {
+                            'title': _t('Nota Crédito'),
+                            'body': _t(`La orden ${refund_order.account_move_name} ya ha sido parcialmente reembolsada por un valor de $ ${monto_reembolsado}. Valor restante para devolucion: $ ${monto_a_reembolsar - monto_reembolsado}`)
+                    });
+                }
+            }
+
+            const order = this._prepare_order_from_order(refund_order);
             this.env.pos.set_order(order);
             order.trigger("change");
             this.showScreen("ProductScreen");
@@ -58,8 +113,14 @@ odoo.define("pos_order_mgmt.RefundOrderButton", function (require) {
 
             // Se prepara refundOrder con los campos de la orden original            
             order.attributes = refund_order.attributes
-            order.Fv = refund_order.Fv
+            order.Resolution = refund_order.Resolution
             order.state = refund_order.state
+
+            // Se agrega la recuperacion de account_move_name con el objetivo de darle a la Nota de Credito 
+            // un campo que la pueda asociar con la orden original. Esto nos permite que al momento de hacer 
+            // la devolucion, se pueda asociar la Nota de Credito con la Factura original y asi poder verificar 
+            // si existen o no devoluciones parciales o totales de la factura original.
+            order.related_order = refund_order.account_move_name
 
             return order;
         }
